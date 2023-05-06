2cd2a3b404916986fc26398c7475e4cbbfcca50d
Giulliana  Yamaguchi <88039867+Giullianayamaguchi@users.noreply.github.com>
2023-05-06 20:42:55 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,12 +1,40 @@
-var BtnSignin = document.querySelector("#signin");
-var btnSignUp = document.querySelector("#signup");
+/*=====================================================================================================================/*
+    Fonte: program.js 
+    Motivo: Responsavel pela lógica e funcionamento do html 'index.html' responsável pelo funcionamento da página
+    Data: 18/04/2023
+    Programadores(As): Giulliana Yamaguchi
+/*=====================================================================================================================*/
 
-var body = document.querySelector("body");
+/*
+===================================
+    Declaração das Variaveis
+===================================*/
 
-BtnSignin.addEventListener("click", function(){
-    body.className = "sign-in-js";
-});
+var BtnSignin = document.querySelector(".signin-btn"); // 
+var btnSignUp = document.querySelector(".signup-btn"); //
 
-btnSignUp.addEventListener("click", function(){
-    body.className = "sign-up-js";
+var tela1 = document.getElementById("Signin"); // recebe id para identificar a tela 1
+var tela2 = document.getElementById("Signup"); // recebe id para identificar a tela 2
+
+ 
+/*====================================================================================================
+    Escopo: Reservado para trocar telas
+    Motivo: Escopo Reservado para fazer a troca de conteúdo da tela ao clicar nos botões 
+    Data: 18/04/2023
+    Programadores(As): Giulliana Yamaguchi
+======================================================================================================*/
+
+btnSignUp.addEventListener("DOMContentLoaded", function() {
+    tela1.classList.remove("visible")
+    tela2.classList.add("visible")
 })
+
+function Entrar(){
+    tela1.classList.remove("visible")
+    tela2.classList.add("visible")
+}
+
+function Cadastrar(){
+    tela2.classList.remove("visible")
+    tela1.classList.add("visible") 
+}
